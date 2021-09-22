17cc955cbb17e79d20d4878d8d966dc8218ce638
furtadomn <mnfurtado@yahoo.com.br>
2021-09-22 18:19:18 -0300

    create components

---
+++ projeto-atual/ceep/src/components/card-nota.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class CardNota extends Component {
+  render() { 
+    return (
+      <section>
+        <header>
+          <h3>Título</h3>
+          <p>Escreva suas notas</p>
+        </header>
+      </section>
+    );
+  }
+}
+ 
+export default CardNota;
